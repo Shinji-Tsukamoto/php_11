d2931a8d3a6e26788d6cd7188e3ebb258bcb3fae
Shinji Tsukamoto <s.tsukamoto4114@gmail.com>
2020-03-17 07:34:17 +0000

    assignment

---
+++ create.blade.2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>MyProfile</title>
+    </head>
+    <body>
+        @extends('layouts.profile')
+        
+        @section('title', 'MY プロフィール')
+        
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>My プロフィール</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
+    </body>
+</html>
